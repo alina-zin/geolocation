c2cc14b0dc478e048269579829ec1a80012aefae
Alina Zinchenko <c0zial00@students.oamk.fi>
2021-02-15 20:31:10 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      Position:
+      {lat.toFixed(3)},
+      {lng.toFixed(3)}
+    </p>
   );
 }
 
